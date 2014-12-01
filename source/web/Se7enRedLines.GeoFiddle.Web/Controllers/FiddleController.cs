74b5de07e4a1c40862ccecd20c10d7b4fbcd8ed7
Borys Korobeinikov <korobeinikov@outlook.com>
2014-12-01 21:35:27 +0200

    initial web project code

---
+++ source/web/Se7enRedLines.GeoFiddle.Web/Controllers/FiddleController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Se7enRedLines.GeoFiddle.Web.Controllers
+{
+    public class FiddleController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ source/web/Se7enRedLines.GeoFiddle.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Se7enRedLines.GeoFiddle.Web.MvcApplication" Language="C#" %>
